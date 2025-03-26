2f23d81af8105b3a321566963e5218a155e37dfd
Danil <75629745+Stamodey@users.noreply.github.com>
2025-03-27 01:44:34 +0300

    Lab_5_task_4_recursive_sum_of_digits

---
+++ recursive_sum_of_digits.fs
@@ -0,0 +1,13 @@
+let rec sumOfDigits num =
+    if num = 0 then 
+        0
+    else 
+        (num % 10) + sumOfDigits (num / 10)
+
+[<EntryPoint>]
+let main avrg = 
+    printf "Введите число: " 
+    let num = int (Console.ReadLine())
+
+    printfn "Сумма цифр числа: %d" (sumOfDigits num)
+    0
